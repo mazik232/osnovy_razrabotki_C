39791fc0c6b25f667dd86d04914383fcff9bf7e6
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-10-03 12:01:32 +0500

    abc

---
+++ 1. Минимальная строка/Минимальная строка.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    string a, b, c;
+    cin >> a >> b >> c;
+    vector<string> minstroka = { a, b, c };
+    auto min = min_element(begin(minstroka), end(minstroka));
+    cout << "min = " << *min;
+}
