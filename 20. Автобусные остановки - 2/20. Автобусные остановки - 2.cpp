1398fba743af74db588f7d2a86802538ecffb18d
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-10-06 16:00:41 +0500

    White

--- 20. Автобусные остановки - 2/20. Автобусные остановки - 2.cpp
+++ 20. Автобусные остановки - 2/20. Автобусные остановки - 2.cpp
@@ -1,8 +1,36 @@
 ﻿
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    int Q;
+    cin >> Q;
+    map<int, vector<string>> bus_stops;
+    int bus_count = 0;
+    for (int i = 0; i < Q; i++) {
+        int num;
+        cin >> num;
+        vector<string> stops(num);
+        for (auto& i : stops) {
+            cin >> i;
+        }
+        bus_count++;
+        for (auto& x : bus_stops) {
+            if (x.second == stops) {
+                cout << "Already exists for " << x.first << endl;
+            }
+            else {
+                bus_stops[bus_count] = stops;
+                cout << "New bus " << bus_count << endl;
+            }
+        }
 
+    }
+}
