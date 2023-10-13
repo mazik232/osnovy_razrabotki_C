32e0a92241db0474108eb5a030503adcd3fb02a7
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-10-13 10:38:00 +0500

    white

--- 22. Множество значений словаря/22. Множество значений словаря.cpp
+++ 22. Множество значений словаря/22. Множество значений словаря.cpp
@@ -1,10 +1,29 @@
 ﻿#include <iostream>
 #include <set>
+#include <map>
 
 using namespace std;
 
+set<string> BuildMapValuesSet(const map<int, string>& m) {
+    set<string> s;
+    for (auto& x : m) {
+        s.insert(x.second);
+    }
+    return s;
+}
+
 int main()
 {
-    
+    set<string> values = BuildMapValuesSet({
+    {1, "odd"},
+    {2, "even"},
+    {3, "odd"},
+    {4, "even"},
+    {5, "odd"}
+        });
+
+    for (const string& value : values) {
+        cout << value << endl;
+    }
 }
 
