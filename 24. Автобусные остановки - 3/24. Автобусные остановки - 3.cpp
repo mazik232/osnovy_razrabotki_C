1436af7e29886dec36e9b1e6caabb923404f2a64
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-10-13 15:25:51 +0500

    white

---
+++ 24. Автобусные остановки - 3/24. Автобусные остановки - 3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+
+int main()
+{
+    int q;
+    int num = 1;
+    cin >> q;
+    map <set<string>, int> stop_buses;
+    while (q > 0) {
+        int n;
+        set<string> stops;
+        cin >> n;
+        for (int i=0; i < n; i++) {
+            string stop;
+            cin >> stop;
+            stops.insert(stop);
+        }
+        if (stop_buses.count(stops) == 1) {
+            cout << "Already exists for " << stop_buses[stops] << endl;
+        }
+        else {
+            stop_buses[stops] = num;
+            cout << "New bus " << num << endl;
+            num++;
+        }
+    }
+}
+
+
