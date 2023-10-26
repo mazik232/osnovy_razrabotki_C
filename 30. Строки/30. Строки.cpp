ff4ccc1087c923135e2fd75d57f2765a85a25eb3
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-10-26 15:22:15 +0500

    white

---
+++ 30. Строки/30. Строки.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class ReversibleString 
+{
+public:
+    ReversibleString(){}
+    ReversibleString(const string r_word)
+    {
+        word = r_word;
+    }
+    void Reverse() {
+        reverse(begin(word), end(word));
+    }
+    string ToString() const {
+        return word;
+    }
+private:
+    string word;
+};
+
+
+
+
+int main()
+{
+    ReversibleString s("live");
+    s.Reverse();
+    cout << s.ToString() << endl;
+
+    s.Reverse();
+    const ReversibleString& s_ref = s;
+    string tmp = s_ref.ToString();
+    cout << tmp << endl;
+
+    ReversibleString empty;
+    cout << '"' << empty.ToString() << '"' << endl;
+
+    return 0;
+
+}
+
