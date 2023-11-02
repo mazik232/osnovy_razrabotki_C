1592ff7b1477e5fb94556ca889f6c29ca58d63ca
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-11-02 12:16:39 +0500

    white

---
+++ 35. Чтение и вывод таблицы/35. Чтение и вывод таблицы.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Readtext(const string& text) {
+    vector<int> values;
+    int N = 0;
+    int M = 0;
+    ifstream input(text);
+    if (input) {
+        string line;
+        getline(input, N, ',');
+        getline(input, M, '/n');
+        while (getline(input, line, ',')) {
+            N = line;
+        }
+    }
+}
+
+int main()
+{
+    const string text = "input.txt";
+    Readtext(text);
+
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+void findAllFlights(map<char, vector<char>>& graph, char startNode, char endNode) {
+    vector<string> results;
+    vector<pair<char, string>> flights;
+    flights.push_back({ startNode, string(1, startNode) });
+    set<char> visited;
+
+    while (!flights.empty()) {
+        pair<char, string> flight = flights.back();
+        cout << "flight nachalo while: " << endl; 
+        cout << "       flight first: " << flight.first << endl;
+        cout << "       flight second: " << flight.second << endl;
+        flights.pop_back();
+
+        if (flight.first == endNode) {
+            results.push_back(flight.second);
+            cout << "results: " << flight.second << endl;
+        }
+
+        for (char neighbor : graph[flight.first]) {
+            if (visited.find(neighbor) == visited.end()) {
+                flights.push_back({ neighbor, flight.second + " -> " + neighbor });
+                visited.insert(neighbor);
+                cout << "neighbor: " << neighbor << ", flight.second: " << flight.second << "  -> neighbor: " << neighbor << endl;
+            }
+        }
+    }
+
+    cout << "All flights from " << startNode << " to " << endNode << ":" << endl;
+    for (string result : results) {
+        cout << result << endl;
+    }
+}
+
+int main() {
+    map<char, vector<char>> graph = {
+        {'A', {'B', 'D', 'E'}},
+        {'B', {'C', 'N', 'Z'}},
+        {'D', {'E', 'F'}},
+        {'F', {'S', 'E', 'D'}}
+    };
+
+    findAllFlights(graph, 'A', 'S');
+
+    return 0;
+}
