e84a45c50060d3e67193e8fc2b8454978cc677fd
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-11-22 16:20:28 +0500

    a

--- 41. Финальная задача white C++/41. Финальная задача white C++.cpp
+++ 41. Финальная задача white C++/41. Финальная задача white C++.cpp
@@ -1,14 +1,20 @@
 ﻿#include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+
 
 class Date {
 public:
     int GetYear() const;
+
     int GetMonth() const;
+
     int GetDay() const;
+
 };
 
 bool operator<(const Date& lhs, const Date& rhs);
@@ -19,9 +25,13 @@ public:
     bool DeleteEvent(const Date& date, const string& event);
     int  DeleteDate(const Date& date);
 
-    Find(const Date& date) const;
+    void Find(const Date& date) const {
 
-    void Print() const;
+    }
+
+    void Print() const {
+
+    }
 
     void DoSomething(const map<int, int>& m) {
         // ...
@@ -34,7 +44,36 @@ int main() {
     Database db;
     string command;
     while (getline(cin, command)) {
-        // Считайте команды с потока ввода и обработайте каждую
+        Date date;
+        string event, str;
+        date.GetYear();
+        date.GetMonth();
+        date.GetDay();
+        if (str == "Add") {
+            db.AddEvent(date, event);
+        }
+        else if (str == "Del") {
+            if (event.empty()) {
+                cout << "Deleted " << db.DeleteDate(date) << " events" << endl;
+            }
+            else {
+                if (db.DeleteEvent(date, event)) {
+                    cout << "Deleted successfully" << endl;
+                }
+                else {
+                    cout << "Event not found" << endl;
+                }
+            }
+        }
+        else if (str == "Find") {
+            db.Find(date);;
+        }
+        else if (str == "Print") {
+            db.Print();
+        }
+        else {
+            cout << "Unknown command: " << str << endl;
+        }
     }
     return 0;
 }
