6c44c1a03a980926492f3d09814688c64b3c2764
Azamat Islamgaliev <azamatik232@yandex.ru>
2023-11-30 11:43:12 +0500

    a

--- 41. Финальная задача white C++/41. Финальная задача white C++.cpp
+++ 41. Финальная задача white C++/41. Финальная задача white C++.cpp
@@ -12,12 +12,17 @@ class Date {
 public:
     Date(string date) {
         stringstream ss(date);
-        char delimiter;
+        char delimiter = '-';
         ss >> year >> delimiter >> month >> delimiter >> day;
-        if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31) {
-            throw invalid_argument("Month value is invalid: ");
+        if (ss.fail() || ss.peek() != EOF) {
+            throw runtime_error("Wrong date format: " + date);
         }
-        cout << year << " " << month << " " << day;
+        if (month < 1 || month > 12) {
+            throw runtime_error("Wrong date format: " + to_string(month));
+        }
+        if (day < 1 || day > 31) {
+            throw runtime_error("Wrong date format: " + to_string(day));
+        }
     }
 
     int GetYear() const {
@@ -82,28 +87,43 @@ int main() {
         ss >> str;
         if (str == "Add") {
             ss >> dateStr >> event;
-            Date date(dateStr);
-            db.AddEvent(date, event);
+            try {
+                Date date(dateStr);
+                db.AddEvent(date, event);
+            }
+            catch (exception& ex) {
+                cout << ex.what() << endl;
+            }
         }
         else if (str == "Del") {
             ss >> dateStr >> event;
-            Date date(dateStr);
-            if (event.empty()) {
-                cout << "Deleted " << db.DeleteDate(date) << " events" << endl;
-            }
-            else {
-                if (db.DeleteEvent(date, event)) {
-                    cout << "Deleted successfully" << endl;
+            try {
+                Date date(dateStr);
+                if (event.empty()) {
+                    cout << "Deleted " << db.DeleteDate(date) << " events" << endl;
                 }
                 else {
-                    cout << "Event not found" << endl;
+                    if (db.DeleteEvent(date, event)) {
+                        cout << "Deleted successfully" << endl;
+                    }
+                    else {
+                        cout << "Event not found" << endl;
+                    }
                 }
             }
+            catch (exception& ex) {
+                cout << ex.what() << endl;
+            }    
         }
         else if (str == "Find") {
             ss >> dateStr;
-            Date date(dateStr);
-            db.Find(date);
+            try {
+                Date date(dateStr);
+                db.Find(date);
+            }
+            catch (exception& ex) {
+                cout << ex.what() << endl;
+            }
         }
         else if (str == "Print") {
             db.Print();
