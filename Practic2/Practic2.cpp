b51c11590e1314d6f617c7a146c2ec4809e5aa6a
Azamat <azamatik232@yandex.ru>
2024-06-05 16:51:58 +0500

    practic

---
+++ Practic2/Practic2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+// Реализуйте здесь
+// * класс Matrix
+// * оператор ввода для класса Matrix из потока istream
+// * оператор вывода класса Matrix в поток ostream
+// * оператор проверки на равенство двух объектов класса Matrix
+// * оператор сложения двух объектов класса Matrix
+
+class Matrix {
+public:
+	Matrix() {
+		num_rows = 0;
+		num_cols = 0;
+	}
+	Matrix(int nums, int cols) {
+		Reset(nums, cols);
+	}
+	void Reset(int nums, int cols) {
+
+	}
+	int At() const {
+
+	}
+	int& At() {
+
+	}
+	int GetNumRows() const {
+		return num_rows;
+	}
+	int GetNumCols() const {
+		return num_cols;
+	}
+private:
+	int num_rows;
+	int num_cols;
+	vector<vector<int>> matrix;
+};
+
+bool operator==() {
+
+}
+Matrix operator+() {
+
+}
+istream operator>>() {
+
+}
+ostream operator<<() {
+
+}
+int main() {
+	Matrix one;
+	Matrix two;
+
+	cin >> one >> two;
+	cout << one + two << endl;
+	return 0;
+}
---
+++ Praktic/Praktic.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <map>
+
+
+using namespace std;
+
+
+int GreatestCommonDivisor(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	else {
+		return GreatestCommonDivisor(b, a % b);
+	}
+}
+
+class Rational
+{
+public:
+	Rational()
+	{
+		p = 0;
+		q = 1;
+	}
+
+	Rational(int numerator, int denominator)
+	{
+		if (denominator == 0) {
+			throw exception();
+		}
+		const int gcd = GreatestCommonDivisor(numerator, denominator);
+		p = numerator / gcd;
+		q = denominator / gcd;
+		if (q < 0) {
+			p = -p;
+			q = -q;
+		}
+	}
+
+	int Numerator() const
+	{
+		return p;
+	}
+
+	int Denominator() const
+	{
+		return q;
+	}
+private:
+	int p;
+	int q;
+};
+
+bool operator==(const Rational& rhs, const Rational& lhs) {
+	if (rhs.Numerator() == lhs.Numerator() && rhs.Denominator() == lhs.Denominator()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+	return {
+		lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator(),
+		lhs.Denominator() * rhs.Denominator()
+	};
+}
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+	return {
+		lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator(),
+		lhs.Denominator() * rhs.Denominator()
+	};
+}
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+	return {
+		lhs.Numerator() * rhs.Numerator(), lhs.Denominator() * rhs.Denominator()
+	};
+}
+Rational operator / (const Rational& lhs, const Rational& rhs) {
+	if (rhs.Numerator() == 0) {
+		throw exception();
+	}
+	return {
+		lhs * Rational(rhs.Denominator(), rhs.Numerator())
+	};
+}
+
+istream& operator >> (istream& is, Rational& r) {
+	int n, d;
+	char c;
+	if (is) {
+		is >> n >> c >> d;
+		if (is && c == '/') {
+			r = Rational(n, d);
+		}
+	}
+	return is;
+}
+
+
+ostream& operator << (ostream& os, const Rational& r) {
+	return os << r.Numerator() << "/" << r.Denominator();
+}
+
+bool operator < (const Rational& lhs, const Rational& rhs) {
+	return (lhs - rhs).Numerator() < 0;
+}
+
+int main()
+{
+	try
+	{
+		Rational r(1, 0);
+		cout << "Doesn't throw in case of zero denominator" << endl;
+		return 1;
+	}
+	catch (invalid_argument&)
+	{
+
+	}
+
+	try
+	{
+		auto x = Rational(1, 2) / Rational(0, 1);
+		cout << "Doesn't throw in case of division by zero" << endl;
+		return 2;
+	}
+	catch (domain_error&)
+	{
+
+	}
+
+	cout << "OK" << endl;
+	return 0;
+}
